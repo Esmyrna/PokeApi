915f91d9a9dd1b3f84fb040e8133cb25cf43cf35
Esmyrna <esmyrnaoliveira13@gmail.com>
2023-05-11 13:49:04 -0300

    Consumo da API

--- src/context/global.tsx
+++ src/context/global.tsx
@@ -1,25 +1,86 @@
-import { useContext, createContext, ReactNode } from "react";
+import { useContext, createContext, ReactNode, useReducer, useEffect } from "react";
+
+// interface para tipar informações do pokemon
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+// interface para tipar as propiedades do meu estado inicial
 
 interface GlobalContextProps {
   value: string;
+  allPokemon: Pokemon[];
+  pokemon: Pokemon;
+  pokemonDataBase: Pokemon[];
+  searchResults: Pokemon[];
+  next: string;
+  loading: boolean;
 }
 
-// Passar o estado inicial para o nosso Context do React.
+// interface para a ação
+
+interface Action {
+  type: string;
+  payload?: any;
+}
 
 const GlobalContext = createContext<GlobalContextProps>({
   value: "",
+  allPokemon: [],
+  pokemon: { name: "", url: "" },
+  pokemonDataBase: [],
+  searchResults: [],
+  next: "",
+  loading: false,
 });
 
-// Provider vai passar as informações para todos os componentes
+// função reducer
+const reducer = (state: GlobalContextProps, action: Action) => {
+  switch (action.type) {
+    // adicionar um caso para cada tipo de ação
+    default:
+      return state;
+  }
+}
 
 export const GlobalProvider = ({ children }: { children: ReactNode }) => {
+
+  const baseUrl = 'https://pokeapi.co/api/v2/'
+
+  const allPokemon = async () => {
+    const res = await fetch(`${baseUrl}pokemon?limit=20`);
+    const data = await res.json();
+    console.log(data)
+  }
+
+  // Renderizar meu componente com os dados da API:
+
+  useEffect(() => {
+    allPokemon();
+  })
+
+  // objeto do meu estado inicial:
+  const initialState: GlobalContextProps = {
+    allPokemon: [],
+    pokemon: { name: "", url: "" },
+    pokemonDataBase: [],
+    searchResults: [],
+    next: "",
+    loading: false,
+    value: "",
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <GlobalContext.Provider value={{ value: 'Hello World' }}>
+    <GlobalContext.Provider value={state}>
       {children}
     </GlobalContext.Provider>
   );
 };
 
 export const useGlobalContext = () => {
-  return useContext(GlobalContext).value;
+  return useContext(GlobalContext);
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { useGlobalContext } from "@/context/global"
 
 
 
 export default function Home() {
+
+  const g = useGlobalContext(); 
+  console.log(g)
   return (
     <>
      
